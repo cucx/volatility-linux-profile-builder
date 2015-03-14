8b139668d580c1de73f653daf12a434b1b55c64d
root <root@localhost.localdomain>
2015-03-14 10:16:37 +0000

    message

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,3 @@
-#!bin/bash
+#!/bin/bash
 
-apt-get install openssl openssl-dev git subversion dwarfdump zip linux-headers-$(uname -r) -y
+apt-get install openssl git subversion dwarfdump zip linux-headers-$(uname -r) -y
